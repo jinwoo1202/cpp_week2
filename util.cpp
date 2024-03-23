4a112a80fea1951446bf2372413ff021be7549eb
82105 <82105@gmail.com>
2024-03-23 19:11:17 +0900

    fix: 정답의 숫자가 중복되지 않도록 변경

--- util.cpp
+++ util.cpp
@@ -7,7 +7,18 @@ using namespace std;
 int genAnswer(){
     mt19937 gen(time(NULL));
 
-    uniform_int_distribution<int> dist(100,999);
+    uniform_int_distribution<int> dist(1,9);
+    int answer=0;
+    int digits[3]={0};
 
-    return dist(gen);
+    for(int i = 0; i<3; i++){
+        int digit = dist(gen);
+        while (digits[0]==digit || digits[1]== digit || digits[2]==digit){
+            digit = dist(gen);
+        }
+        digits[i]=digit;
+        answer = answer*10+digit;
+    }
+
+    return answer;
 }
